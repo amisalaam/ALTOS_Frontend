f1f92a25cedde0c86109d3ad85198fc498ca5ead
amisalaam <amisalaam9@gmail.com>
2024-07-31 00:51:02 +0530

    working on dark mode toggle

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import LoginComponent from '../components/authentication/Login'
+
+function Login() {
+  return (
+    <div>
+        <Navbar/>
+        <LoginComponent/>
+    </div>
+  )
+}
+
+export default Login
